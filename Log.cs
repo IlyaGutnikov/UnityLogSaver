6fabbf4b362e30fee7918cbfe26b89c93c68b56d
Ilya Gutnikov <ilya.gutnikov@dtigames.com>
2018-05-28 17:59:06 +0300

    Created data structures

---
+++ Log.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace IlyaGutnikov.LogSaver
+{
+    [Serializable]
+    public class Log
+    {
+        public string Condition;
+        public string StackTrace;
+        public LogType LogType;
+
+        public string DateTime;
+
+        public Log(string condition, string stackTrace, LogType type, string dateTime)
+        {
+            Condition = condition;
+            StackTrace = stackTrace;
+            LogType = type;
+            DateTime = dateTime;
+        }
+    }
+}
---
+++ LogsKeeper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IlyaGutnikov.LogSaver
+{
+    [Serializable]
+    public class LogsKeeper
+    {
+        public List<Log> Logs;
+    }
+}
